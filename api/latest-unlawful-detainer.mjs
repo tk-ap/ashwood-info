867be23138a48d040ad4f07b1a1092365a7a0a7b
tk-ap <164285623+tk-ap@users.noreply.github.com>
2026-09-25 08:46:48 -0700

    fix(workspace): remove extra serverless function for UD link

--- api/latest-unlawful-detainer.mjs
+++
@@ -1,8 +0,0 @@
-const LATEST_UD_PDF_URL = 'https://drive.google.com/file/d/17xoV4qzS6Tr0CA-kgWbfgo_MFVBYUa7b/view?usp=drivesdk';
-
-export default function handler(req,res){
-  res.statusCode=302;
-  res.setHeader('location',LATEST_UD_PDF_URL);
-  res.setHeader('cache-control','no-store, max-age=0');
-  return res.end();
-}
